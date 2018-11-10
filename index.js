eb9b079231dee0358a2d11a094adaf7b1a678cf1
Matt Hoenecke <mhoenecke@gmail.com>
2018-11-10 14:10:03 -0800

    add images and cleans some code up

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
-window.onload = function () {
-  const quoteSpan = document.getElementById('quote-text');
-  const authorSpan = document.getElementById('quote-author');
-  const backgroundContainer = document.getElementById('background');
-  let oldQuoteIndex = -1;
-  let newQuoteIndex = -1;
-  const clickButton = document.getElementById('new-quote-button');
-  const quotes = [
-    {
-      author: 'Batman',
-      text: 'The hero Gotham needs, not the one it deserves.',
-      url: 'batman.jpg'
-    }, {
-      author: 'Dr. Frankenstein',
-      text: "It's Alive! It's Alive!",
-      url: 'frankenstein.jpg'
-    }, {
-      author: 'Darth Vader',
-      text: "Luke, I am your father.",
-      url: 'darthvader.jpg'
-    }
-  ]
+// html elements
+const quoteSpan = document.getElementById('quote-text');
+const authorSpan = document.getElementById('quote-author');
+const backgroundContainer = document.getElementById('background');
+const clickButton = document.getElementById('new-quote-button');
 
-  function newQuote() {
-    while (newQuoteIndex === oldQuoteIndex) {
-      newQuoteIndex = Math.floor(Math.random() * quotes.length);
-    }
-    backgroundContainer.style.backgroundImage = quotes[newQuoteIndex].url;
-    quoteSpan.innerHTML = quotes[newQuoteIndex].text;
-    authorSpan.innerHTML = ' - ' + quotes[newQuoteIndex].author;
+// Constant
+const QUOTES = [
+  {
+    author: 'Batman',
+    text: 'The hero Gotham needs, not the one it deserves.',
+    url: 'batman2.jpg'
+  }, {
+    author: 'Dr. Frankenstein',
+    text: "It's Alive! It's Alive!",
+    url: 'frankenstein.jpg'
+  }, {
+    author: 'Darth Vader',
+    text: "Luke, I am your father.",
+    url: 'vadar.jpg'
+  }
+]
 
-    oldQuoteIndex = newQuoteIndex;
+// Initialize on start
+let oldQuoteIndex = -1;
+let newQuoteIndex = -1;
+clickButton.onclick = newQuote;
+
+// Make quote appear on load
+newQuote();
+
+function newQuote() {
+  while (newQuoteIndex === oldQuoteIndex) {
+
+    // Random quote index
+    newQuoteIndex = Math.floor(Math.random() * QUOTES.length);
   }
 
-  clickButton.onclick = newQuote;
+  // Assign markup to the right information
+  backgroundContainer.style.backgroundImage = `url('${QUOTES[newQuoteIndex].url}')`;
+  quoteSpan.classList.add('fade-out');
+  quoteSpan.innerHTML = QUOTES[newQuoteIndex].text;
+  quoteSpan.classList.remove('fade-out');
+  authorSpan.innerHTML = ' - ' + QUOTES[newQuoteIndex].author;
+
+  oldQuoteIndex = newQuoteIndex;
 }
+
+
